26548016c386d05b761e666fc65e511f07adc03f
Derryck <oseiagyemanb@gmail.com>
2023-06-06 17:09:30 +0100

    update of cartButton file

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context';
 
 const CartButtons = () => {
 	const { closeSidebar } = useProductsContext();
-	const { total_items } = useCartContext();
+	const { total_items, clearCart } = useCartContext();
 	const { loginWithRedirect, myUser, logout } = useUserContext();
 	return (
 		<Wrapper className="cart-btn-wrapper">
@@ -23,7 +23,10 @@ const CartButtons = () => {
 				<button
 					type="button"
 					className="auth-btn"
-					onClick={() => logout({ returnTo: window.location.origin })}
+					onClick={() => {
+						clearCart();
+						logout({ returnTo: window.location.origin });
+					}}
 				>
 					Logout <FaUserMinus />
 				</button>
