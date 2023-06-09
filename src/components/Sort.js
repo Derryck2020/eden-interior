0244b14b76670f85a048e72b1a41a61af43349d2
Derryck <oseiagyemanb@gmail.com>
2023-06-09 02:47:05 +0100

    fixing of bug

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -42,8 +42,8 @@ const Sort = () => {
 				>
 					<option value="price-lowest">price (lowest)</option>
 					<option value="price-highest">price (highest)</option>
-					<option value="name-a">name (a-z)</option>
-					<option value="name-z">name (z-a)</option>
+					<option value="name-a">name (a - z)</option>
+					<option value="name-z">name (z - a)</option>
 				</select>
 			</form>
 		</Wrapper>
