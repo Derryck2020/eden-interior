d91e6229c04799d8d715341d5a1ee1cb518bc139
Derryck <oseiagyemanb@gmail.com>
2023-06-11 10:40:04 +0100

    update of paragraph

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -11,15 +11,37 @@ const AboutPage = () => {
 				<img src={aboutImg} alt="slik relaxer" />
 				<article>
 					<div className="title">
-						<h2>Our Growth</h2>
+						<h2>History</h2>
 						<div className="underline"></div>
 					</div>
 					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Perferendis voluptates provident expedita natus aliquid
-						ducimus! Deserunt nam dolores quaerat, labore quas in. Animi
-						exercitationem blanditiis non iusto vitae quia ad aspernatur
-						sed. Repellendus, sunt quam?
+						With a passion for transforming living spaces into exquisite
+						sanctuaries, eDEN Interiors has been dedicated to providing
+						unrivaled design solutions for homeowners worldwide. From
+						humble beginnings as a small local business, eDEN Interiors
+						rapidly expanded its reach, captivating clients with its
+						innovative and sophisticated approach.{' '}
+						<p>
+							Throughout the years, eDEN Interiors has remained at the
+							forefront of interior design trends, embracing timeless
+							elegance and contemporary flair. The company's team of
+							talented designers and skilled craftsmen has consistently
+							delivered exceptional results, elevating residences into
+							personalized works of art. eDEN Interiors' commitment to
+							excellence has earned it numerous accolades, solidifying
+							its reputation as a trusted industry leader.{' '}
+						</p>
+						<p>
+							{' '}
+							Today, eDEN Interiors continues to create breathtaking
+							environments that inspire and enrich lives. From concept to
+							execution, their holistic approach ensures every detail is
+							meticulously crafted, resulting in extraordinary spaces
+							that reflect the unique vision and style of each client.
+							With an unwavering commitment to unparalleled quality and
+							client satisfaction, eDEN Interiors continues to redefine
+							the art of home interiors.
+						</p>
 					</p>
 				</article>
 			</Wrapper>
