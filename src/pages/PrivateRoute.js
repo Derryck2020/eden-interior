fab9b3f1ff39286079dc30520d8cb0893d76bccd
Derryck <oseiagyemanb@gmail.com>
2023-06-12 05:13:20 +0100

    update of file

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 // will remove later
 // import { useUserContext } from '../context/user_context';
 
-const PrivateRoute = ({ children, ...rest }) => {
+const PrivateRoute = ({ children }) => {
 	const { user } = useAuth0();
-	return (
-		<Route
-			{...rest}
-			render={() => {
-				return user ? children : <Redirect to="/"></Redirect>;
-			}}
-		></Route>
-	);
+	if (!user) {
+		return <Navigate to="/" />;
+	}
+	return children;
 };
 export default PrivateRoute;
