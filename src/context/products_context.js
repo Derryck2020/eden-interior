c3d25222854e95cf5e985c529be2ed89dbeb9db8
Xir Derryck <88989982+Derryck2020@users.noreply.github.com>
2024-04-24 11:01:56 +0000

    Update products_context.js

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -40,7 +40,7 @@ export const ProductsProvider = ({ children }) => {
 	const fetchProducts = async (url) => {
 		dispatch({ type: GET_PRODUCTS_BEGIN });
 		try {
-			const response = await axios.get(`https://crossorigin.me/${url}`);
+			const response = await axios.get(`${url}`);
 			const products = response.data;
 			dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
 		} catch (error) {
